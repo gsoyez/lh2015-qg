50a436102346f987da1b0b66498eb85a6dbdce08
Gregory Soyez <soyez@fastjet.fr>
2015-06-05 22:55:41 +0200

    updated the process script

--- post-process.sh
+++ post-process.sh
@@ -6,31 +6,53 @@
 echo "Computing separations"
 echo "  Pythia"
 for fn in Pythia/results/qq*.yoda; do
-    outname=${fn/qq/sep}
-    if [ ! -f ${outname} ] || [ ! -z $FORCE ]; then
-        ./compute-efficiencies.py $fn ${fn/qq/gg} $outname > ${outname%yoda}log
+    sepname=${fn/qq/sep}
+    gluname=${fn/qq/gg}
+    if [ -f ${gluname} ]; then
+        if [ ! -f ${sepname} ] || [ ! -z $FORCE ]; then
+            ./compute-efficiencies.py $fn ${gluname} $sepname > ${sepname%yoda}log
+        fi
     fi
 done
 
 echo "  Vincia"
 for fn in Vincia/results/qq*.yoda; do
-    outname=${fn/qq/sep}
-    if [ ! -f ${outname} ] || [ ! -z $FORCE ]; then
-        ./compute-efficiencies.py $fn ${fn/qq/gg} $outname > ${outname%yoda}log
+    sepname=${fn/qq/sep}
+    gluname=${fn/qq/gg}
+    if [ -f ${gluname} ]; then
+        if [ ! -f ${sepname} ] || [ ! -z $FORCE ]; then
+            ./compute-efficiencies.py $fn ${gluname} $sepname > ${sepname%yoda}log
+        fi
     fi
 done
 
 echo "  Herwig"
 for fn in Herwig/Herwig7/Parton_level_Results/*_u_u_*.yoda; do
-    outname=${fn/_u_u_/_sep_}
-    if [ ! -f ${outname} ] || [ ! -z $FORCE ]; then
-        ./compute-efficiencies.py $fn ${fn/_u_u_/_g_g_} $outname > ${outname%yoda}log
+    sepname=${fn/_u_u_/_sep_}
+    gluname=${fn/_g_g_/_sep_}
+    if [ -f ${gluname} ]; then
+        if [ ! -f ${sepname} ] || [ ! -z $FORCE ]; then
+            ./compute-efficiencies.py $fn ${gluname} $sepname > ${sepname%yoda}log
+        fi
+    fi
+
+done
+
+echo "  Sherpa"
+for fn in Sherpa/results/q*.yoda; do
+    sepname=${fn/result\/q/result\/sep}
+    gluname=${fn/result\/q/result\/g}
+    if [ -f ${gluname} ]; then
+        if [ ! -f ${sepname} ] || [ ! -z $FORCE ]; then
+            ./compute-efficiencies.py $fn ${gluname} $sepname > ${sepname%yoda}log
+        fi
     fi
 done
 
 # rebin pythia, vincia, herwig
 echo "Rebinning"
 echo "  Pythia"
+mkdir -p Pythia/rebinned
 for fn in Pythia/results/*.yoda; do
     outname=${fn/results/rebinned} 
     if [ ! -f ${outname} ] || [ ! -z $FORCE ]; then
@@ -39,6 +61,7 @@ for fn in Pythia/results/*.yoda; do
 done
 
 echo "  Vincia"
+mkdir -p Vincia/rebinned
 for fn in Vincia/results/*.yoda; do
     outname=${fn/results/rebinned} 
     if [ ! -f ${outname} ] || [ ! -z $FORCE ]; then
@@ -47,8 +70,18 @@ for fn in Vincia/results/*.yoda; do
 done
 
 echo "  Herwig"
+mkdir -p Herwig/Herwig7/Parton_level_Rebinned
 for fn in Herwig/Herwig7/Parton_level_Results/*.yoda; do
     outname=${fn/Results/Rebinned}
+    if [ ! -f ${outname} ] || [ ! -z $FORCE ]; then
+        ./yoda-rebin.py $fn $outname | grep -v "vs."
+    fi
+done
+
+echo "  Sherpa"
+mkdir -p Sherpa/rebinned
+for fn in Sherpa/results/*.yoda; do
+    outname=${fn/results/rebinned}
     if [ ! -f ${outname} ] || [ ! -z $FORCE ]; then
         ./yoda-rebin.py $fn $outname | grep -v "vs."
     fi
@@ -65,8 +98,11 @@ else
     rivet-mkhtml Pythia/rebinned/qq-200-parton-mec.yoda:Pythia8 \
                  Pythia/rebinned/qq-200-parton.yoda:Pythia8-noME \
                  Vincia/rebinned/qq-200-parton.yoda:Vincia \
+                 Vincia/rebinned/qq-200-parton-nlo.yoda:Vincia-NLO \
                  Herwig/Herwig7/Parton_level_Rebinned/LEP-Matchbox_mum_mup_to_u_u_MG_E200.yoda:Herwig++ \
                  Herwig/Herwig7/Parton_level_Rebinned/LEP-Matchbox_mum_mup_to_u_u_MG_dip_E200.yoda:Herwig++-dip \
+                 Sherpa/rebinned/q200-njet0.yoda:Sherpa-Njet0 \
+                 Sherpa/rebinned/q200-njet2.yoda:Sherpa-Njet2 \
                  -o plots/MCdep-q200-parton >> plots.log 2>&1
 fi
 
@@ -79,6 +115,8 @@ else
                  Vincia/rebinned/gg-200-parton.yoda:Vincia \
                  Herwig/Herwig7/Parton_level_Rebinned/LEP-Matchbox_mum_mup_to_g_g_MG_E200.yoda:Herwig++ \
                  Herwig/Herwig7/Parton_level_Rebinned/LEP-Matchbox_mum_mup_to_g_g_MG_dip_E200.yoda:Herwig++-dip \
+                 Sherpa/rebinned/g200-njet0.yoda:Sherpa-Njet0 \
+                 Sherpa/rebinned/g200-njet2.yoda:Sherpa-Njet2 \
                  -o plots/MCdep-g200-parton >> plots.log 2>&1
 fi
 
@@ -91,6 +129,8 @@ else
                  Vincia/rebinned/sep-200-parton.yoda:Vincia \
                  Herwig/Herwig7/Parton_level_Rebinned/LEP-Matchbox_mum_mup_to_sep_MG_E200.yoda:Herwig++ \
                  Herwig/Herwig7/Parton_level_Rebinned/LEP-Matchbox_mum_mup_to_sep_MG_dip_E200.yoda:Herwig++-dip \
+                 Sherpa/rebinned/sep200-njet0.yoda:Sherpa-Njet0 \
+                 Sherpa/rebinned/sep200-njet2.yoda:Sherpa-Njet2 \
                  -o plots/MCdep-sep200-parton >> plots.log 2>&1
 fi
 
