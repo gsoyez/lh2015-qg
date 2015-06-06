0d714af85b5473dfb6dbb174e6ba533cf7974d09
Gregory Soyez <soyez@fastjet.fr>
2015-06-06 15:05:09 +0200

    produced plots for all R values

--- produce-separation-plots.py
+++ produce-separation-plots.py
@@ -19,15 +19,17 @@ if len(sys.argv) != 3:
 table = sys.argv[1]
 
 measures=["grej20", "grej50", "qrej20", "qrej50", "srej", "I", "I2"]
-observables=["GA_00_00_R6", "GA_20_00_R6", "GA_10_05_R6", "GA_10_10_R6", "GA_10_20_R6"]
+Rvalues=["R3","R6","R9"]
+observables=["GA_00_00", "GA_20_00", "GA_10_05", "GA_10_10", "GA_10_20"]
 
 scatters=[]
 for measure in measures:
-    separation_scatter = yoda.Scatter2D(title=measure, path="/separation/"+measure)
-    for observable,index in zip(observables,range(0,len(observables))):
-        command="./get-separation.sh "+table+" "+observable+" "+measure
-        separation_scatter.addPoint(index, float(os.popen(command).read().rstrip()), xerrs=0.5)
-    scatters.append(separation_scatter)
+    for Rval in Rvalues:
+        separation_scatter = yoda.Scatter2D(title=measure, path="/separation/"+measure+"_"+Rval)
+        for observable,index in zip(observables,range(0,len(observables))):
+            command="./get-separation.sh "+table+" "+observable+"_"+Rval+" "+measure
+            separation_scatter.addPoint(index, float(os.popen(command).read().rstrip()), xerrs=0.5)
+        scatters.append(separation_scatter)
 
 
 yoda.write(scatters,sys.argv[2])
