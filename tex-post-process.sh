63763b6e6ba1a075dc0bb105e2a4aa8e787f3300
Jesse Thaler <jthaler@mit.edu>
2015-07-26 15:38:04 -0400

    Updating tex with Pythia/Vinica parton-level plots

--- tex-post-process.sh
+++ tex-post-process.sh
@@ -15,6 +15,10 @@ cp plots/sum-200-R06-Sherpa-2.1.1-hadron/separation/I2_R6.pdf $texfigdir/I2_R6_h
 cp plots/sum-200-R06-Vincia-1201-hadron/separation/I2_R6.pdf  $texfigdir/I2_R6_hadron_vincia.pdf
 cp plots/sum-200-R06-Herwig-2_7_1-hadron/separation/I2_R6.pdf $texfigdir/I2_R6_hadron_herwig.pdf
 
+cp plots/sum-200-R06-Pythia-8205-parton/separation/I2_R6.pdf  $texfigdir/I2_R6_parton_pythia.pdf
+cp plots/sum-200-R06-Vincia-1201-parton/separation/I2_R6.pdf  $texfigdir/I2_R6_parton_vincia.pdf
+cp plots/sum-200-R06-Herwig-2_7_1-parton/separation/I2_R6.pdf $texfigdir/I2_R6_parton_herwig.pdf
+
 cp plots/modulations-hadron/Qdependence/I2_GA_10_05.pdf      $texfigdir/I2_GA_10_05_hadron_Qdep.pdf
 cp plots/modulations-hadron/Rdependence/I2_GA_10_05.pdf      $texfigdir/I2_GA_10_05_hadron_Rdep.pdf
 cp plots/modulations-hadron/alphasdependence/I2_GA_10_05.pdf $texfigdir/I2_GA_10_05_hadron_alphadep.pdf
