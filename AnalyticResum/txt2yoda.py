2f8515ec8023968eb8e44c0e9dfeb930d7d6cccc
Gregory Soyez <soyez@lpthe.jussieu.fr>
2015-09-14 21:10:46 +0200

    added a draft for a conversion script

---
+++ AnalyticResum/txt2yoda.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#
+# export Jesse's 2-column format (x-y) into a yoda scatter plot
+
+import sys,re,yoda,os
+
+if len(sys.argv) != 3:
+    sys.exit("Usage: txt2yoda.py input_txt_file output_yoda_file")
+
+# open the files
+input_filename  = sys.argv[1]
+output_filename = sys.argv[2]
+
+f = open(input_filename, 'r')
+yoda_scatter = yoda.Scatter2D(title="my_distribution", path="/MC_LHQG_EE/GA_00_00_R10")
+
+for line in f:
+    tokens=line.split()
+    yoda_scatter.addPoint(float(tokens[0]), float(tokens[1]), xerrs=0.0005)
+
+yoda.write(yoda_scatter, output_filename)
