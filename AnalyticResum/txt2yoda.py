4afda3beba171a215db9f4c6c518a8b9c1abe374
Gregory Soyez <soyez@lpthe.jussieu.fr>
2015-09-15 12:41:31 +0200

    merged different beta and R values in a single yoda file (warning: interface change)

--- AnalyticResum/txt2yoda.py
+++ AnalyticResum/txt2yoda.py
@@ -1,21 +1,50 @@
 #!/usr/bin/env python
 #
-# export Jesse's 2-column format (x-y) into a yoda scatter plot
+# export Jesse's 2-column format (x-y) into a yoda scatter plot.  It
+# also merges mutiple beta and R valeus in a single yoda file in order
+# to reach the same format as the output of the MC_QG_EE Rivet
+# analysis.
+#
+# Usage:
+#
+#   txt2yoda input_basename
+#
+# where th basename includes the event level (parton or hadron), the
+# parton type (uu or gg), an optional flag and the Q. Examples:
+#   parton/uu-200
+#   hadron/gg-nogqq-50
 
 import sys,re,yoda,os
 
-if len(sys.argv) != 3:
-    sys.exit("Usage: txt2yoda.py input_txt_file output_yoda_file")
+if len(sys.argv) != 2:
+    sys.exit("Usage: txt2yoda.py input_basename\nExample basename parton/uu-nogqq")
 
-# open the files
-input_filename  = sys.argv[1]
-output_filename = sys.argv[2]
+# filename info
+base = sys.argv[1]
+output_filename = base+".yoda"
 
-f = open(input_filename, 'r')
-yoda_scatter = yoda.Scatter2D(title="my_distribution", path="/MC_LHQG_EE/GA_00_00_R10")
+# the R and beta values we shall loop over
+Rvalues=["02", "04", "06", "08", "10"]
+betas=["05", "10", "20"]
 
-for line in f:
-    tokens=line.split()
-    yoda_scatter.addPoint((float(tokens[0]) + float(tokens[1]))/2.0, float(tokens[2]), xerrs=(float(tokens[1]) - float(tokens[0]))/2.0)
+# an array for all the scatter plots
+scatters=[]
+
+# loop over the beta and R values
+for beta in betas:
+    for Rval in Rvalues:
+        # construct the input filename and open it
+        input_filename  = base+"-beta"+beta+"-R"+Rval+".txt"
+        f = open(input_filename, 'r')
+
+        # declare the yoda 2D scatter
+        yoda_scatter = yoda.Scatter2D(path="/MC_LHQG_EE/GA_10_"+beta+"_R"+str(int(Rval)))
+
+        # parse the input file and construct the scatter plot
+        for line in f:
+            tokens=line.split()
+            yoda_scatter.addPoint((float(tokens[0]) + float(tokens[1]))/2.0, float(tokens[2]), xerrs=(float(tokens[1]) - float(tokens[0]))/2.0)
+
+        scatters.append(yoda_scatter)
 
 yoda.write(yoda_scatter, output_filename)
