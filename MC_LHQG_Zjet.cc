ca47099e1aca13fb5eb4d51bd99b1c590a2cfe2e
Gregory Soyez <gregory.soyez@cea.fr>
2015-10-23 11:24:58 +0200

    fixed a few trivial issues; added mMDT distributions

--- MC_LHQG_Zjet.cc
+++ MC_LHQG_Zjet.cc
@@ -10,6 +10,7 @@
 
 #include "fastjet/JetDefinition.hh"
 #include "fastjet/ClusterSequence.hh"
+#include "fastjet/contrib/ModifiedMassDropTagger.hh"
 using namespace fastjet;
 
 
@@ -54,7 +55,8 @@ namespace Rivet {
         JET_PTMIN_FRACTION(0.6),
         LOG_SCALE_MAX(15.0),
         nRADII(5),
-        DELTA_RADII(0.2){}
+        DELTA_RADII(0.2)
+    {}
     
     /// Book histograms and initialise projections before the run
     void init() {
@@ -71,19 +73,27 @@ namespace Rivet {
       jet_input.vetoNeutrinos();
       jet_input.addVetoPairId(PID::MUON);
       addProjection(jet_input, "JET_INPUT");
-                    
+
+      // mMDT
+      mmdt.reset(new contrib::ModifiedMassDropTagger(0.1));
+      mmdt->set_grooming_mode();
+      mmdt->set_reclustering(true);
+
+      // shape parameter definitions
       _kappa_betas.push_back(pair_double(0.0, 0.0));
       _kappa_betas.push_back(pair_double(1.0, 0.5));
       _kappa_betas.push_back(pair_double(1.0, 1.0));
       _kappa_betas.push_back(pair_double(1.0, 2.0));
       _kappa_betas.push_back(pair_double(2.0, 0.0));
-      
+
+      // histogram bbookings
       for (unsigned int iR=1;iR<nRADII+1; iR++){
         double R=DELTA_RADII*iR;
         ostringstream Rlabel;
         Rlabel << "_R" << (int)(10*R);
         string Rlab = Rlabel.str();
 
+        // plain jet quantities
         _gas.push_back(HistogramHolder(bookHisto1D("GA_00_00"+Rlab,151,-0.5,150.5),
                                        bookHisto1D("log_GA_00_00"+Rlab,300,0,6)));
         _gas.push_back(HistogramHolder(bookHisto1D("GA_10_05"+Rlab, 500, 0.0, 1.0),
@@ -94,6 +104,18 @@ namespace Rivet {
                                        bookHisto1D("log_GA_10_20"+Rlab, 500, -LOG_SCALE_MAX, 0.0)));
         _gas.push_back(HistogramHolder(bookHisto1D("GA_20_00"+Rlab, 500, 0.0, 1.0),
                                        bookHisto1D("log_GA_20_00"+Rlab, 500, -LOG_SCALE_MAX, 0.0)));
+
+        // mMDT quantities
+        _gas.push_back(HistogramHolder(bookHisto1D("mMDT_GA_00_00"+Rlab,151,-0.5,150.5),
+                                       bookHisto1D("log_mMDT_GA_00_00"+Rlab,300,0,6)));
+        _gas.push_back(HistogramHolder(bookHisto1D("mMDT_GA_10_05"+Rlab, 500, 0.0, 1.0),
+                                       bookHisto1D("log_mMDT_GA_10_05"+Rlab, 500, -LOG_SCALE_MAX, 0.0)));
+        _gas.push_back(HistogramHolder(bookHisto1D("mMDT_GA_10_10"+Rlab, 500, 0.0, 1.0),
+                                       bookHisto1D("log_mMDT_GA_10_10"+Rlab, 500, -LOG_SCALE_MAX, 0.0)));
+        _gas.push_back(HistogramHolder(bookHisto1D("mMDT_GA_10_20"+Rlab, 500, 0.0, 1.0),
+                                       bookHisto1D("log_mMDT_GA_10_20"+Rlab, 500, -LOG_SCALE_MAX, 0.0)));
+        _gas.push_back(HistogramHolder(bookHisto1D("mMDT_GA_20_00"+Rlab, 500, 0.0, 1.0),
+                                       bookHisto1D("log_mMDT_GA_20_00"+Rlab, 500, -LOG_SCALE_MAX, 0.0)));
       }
     }
 
@@ -119,7 +141,6 @@ namespace Rivet {
       }
 
       unsigned int ngas = _kappa_betas.size();
-      vector<double> gas(ngas);
 
       // loop over jet radii
       for (unsigned int iR=0;iR<nRADII; iR++){
@@ -127,37 +148,20 @@ namespace Rivet {
         //
         // we'll keep jets above a certain fraction of the total energy 
         double R=DELTA_RADII*(iR+1);
-        JetDefinition jet_def(antikt_algorithm, R, WTA_modp_scheme);
+        JetDefinition jet_def(antikt_algorithm, R, WTA_pt_scheme);
         vector<PseudoJet> jets = SelectorPtMin(ptmin_jet)(jet_def(particles));
       
         if(!jets.size()) continue;
 
-        // loop over the jets
-        foreach (const PseudoJet &jet, jets){
-          // init angularity sums to 0
-          for(unsigned int i=0; i<ngas;i++) gas[i]=0.0;
+        // select only the hardest jet
+        PseudoJet jet = (SelectorNHardest(1)(jets))[0];
+        PseudoJet mmdt_jet = (*mmdt)(jet);
 
-          // sum over the constituents
-          foreach (const PseudoJet& p, jet.constituents()){
-            double pt = p.pt();
-            double theta = p.squared_distance(jet);
-            for(unsigned int i=0; i<ngas;i++)
-              gas[i]+=pow(pt, _kappa_betas[i].first) * pow(theta, 0.5*_kappa_betas[i].second);
-          }
-
-          // normalise and fill histograms
-          for(unsigned int i=0; i<ngas;i++){ 
-            gas[i]/=(pow(jet.pt(), _kappa_betas[i].first) * pow(R, _kappa_betas[i].second));
-            assert(ngas*iR+i < _gas.size());
-            _gas[ngas*iR+i].h->fill(gas[i], weight);
-            // for the log binning make sure we avoid taking log(0) and put
-            // that in the most -ve bin.
-            _gas[ngas*iR+i].h_log->fill(gas[i]>0 ? log(gas[i]) : 1e-5-LOG_SCALE_MAX, weight);
-          }
-        } // loop over jets
+        compute_and_record(jet,      R, ngas*(2*iR),   weight);
+        compute_and_record(mmdt_jet, R, ngas*(2*iR+1), weight);
+        
       } // loop over radii
-    
-     
+      
     }
      
     /// Normalise histograms etc., after the run
@@ -172,9 +176,38 @@ namespace Rivet {
   private:
     vector<pair_double> _kappa_betas;
     vector<HistogramHolder> _gas;
+    
+    SharedPtr<contrib::ModifiedMassDropTagger> mmdt;
 
+
+    void compute_and_record(const PseudoJet &jet, double R, unsigned int offset, double weight){
+      unsigned int ngas = _kappa_betas.size();
+      vector<double> gas(ngas);
+
+      // init angularity sums to 0
+      for(unsigned int i=0; i<ngas;i++) gas[i]=0.0;
+
+      // sum over the constituents
+      foreach (const PseudoJet& p, jet.constituents()){
+        double pt = p.pt();
+        double theta = p.squared_distance(jet);
+        for(unsigned int i=0; i<ngas;i++)
+          gas[i]+=pow(pt, _kappa_betas[i].first) * pow(theta, 0.5*_kappa_betas[i].second);
+      }
+      
+      // normalise and fill histograms
+      for(unsigned int i=0; i<ngas;i++){ 
+        gas[i]/=(pow(jet.pt(), _kappa_betas[i].first) * pow(R, _kappa_betas[i].second));
+
+        //assert(ngas*iR+i < _gas.size());
+        _gas[offset+i].h->fill(gas[i], weight);
+        // for the log binning make sure we avoid taking log(0) and put
+        // that in the most -ve bin.
+        _gas[offset+i].h_log->fill(gas[i]>0 ? log(gas[i]) : 1e-5-LOG_SCALE_MAX, weight);
+      }
+    }
   };
-
+  
 
 
   // Hook for the plugin system
