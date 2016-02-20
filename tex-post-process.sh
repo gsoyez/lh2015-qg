71ee328e732acfb2687a6d0bb3dfb3fbeffdd16f
Jesse Thaler <jthaler@mit.edu>
2016-02-20 18:25:10 -0500

    Putting in plots for Ariadne, minor changes to start to address Gregory's suggestions

--- tex-post-process.sh
+++ tex-post-process.sh
@@ -21,16 +21,16 @@ cp plots/sum-200-R06-Pythia-8205-hadron/separation/I2_R6.pdf  $texfigdir/quarkgl
 cp plots/sum-200-R06-Sherpa-2.1.1-hadron/separation/I2_R6.pdf $texfigdir/quarkgluon_fig_I2_R6_hadron_sherpa.pdf
 cp plots/sum-200-R06-Vincia-1201-hadron/separation/I2_R6.pdf  $texfigdir/quarkgluon_fig_I2_R6_hadron_vincia.pdf
 cp plots/sum-200-R06-Herwig-2_7_1-hadron/separation/I2_R6.pdf $texfigdir/quarkgluon_fig_I2_R6_hadron_herwig.pdf
+cp plots/sum-200-R06-Ariadne-hadron/separation/I2_R6.pdf      $texfigdir/quarkgluon_fig_I2_R6_hadron_ariadne.pdf
 
 #cp plots/sum-200-R06-AnalyticResum-hadron/separation/I2_R6.pdf $texfigdir/quarkgluon_fig_I2_R6_hadron_analytic.pdf
 #cp plots/sum-200-R06-AnalyticResum-parton/separation/I2_R6.pdf $texfigdir/quarkgluon_fig_I2_R6_parton_analytic.pdf
-
 
 cp plots/sum-200-R06-Pythia-8205-parton/separation/I2_R6.pdf  $texfigdir/quarkgluon_fig_I2_R6_parton_pythia.pdf
 cp plots/sum-200-R06-Sherpa-2.1.1-parton/separation/I2_R6.pdf $texfigdir/quarkgluon_fig_I2_R6_parton_sherpa.pdf
 cp plots/sum-200-R06-Vincia-1201-parton/separation/I2_R6.pdf  $texfigdir/quarkgluon_fig_I2_R6_parton_vincia.pdf
 cp plots/sum-200-R06-Herwig-2_7_1-parton/separation/I2_R6.pdf $texfigdir/quarkgluon_fig_I2_R6_parton_herwig.pdf
-cp plots/sum-200-R06-Deductor-1.0.2-parton/separation/I2_R6.pdf $texfigdir/quarkgluon_fig_I2_R6_parton_deductor.pdf
+cp plots/sum-200-R06-Ariadne-parton/separation/I2_R6.pdf      $texfigdir/quarkgluon_fig_I2_R6_parton_ariadne.pdf
 
 cp plots/modulations-hadron/Qdependence/I2_GA_10_05.pdf      $texfigdir/quarkgluon_fig_I2_GA_10_05_hadron_Qdep.pdf
 cp plots/modulations-hadron/Rdependence/I2_GA_10_05.pdf      $texfigdir/quarkgluon_fig_I2_GA_10_05_hadron_Rdep.pdf
