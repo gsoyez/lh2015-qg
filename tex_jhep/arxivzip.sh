63c0fa3f57e14e0b04105b4563de0e34b91c4d3d
Jesse Thaler <jthaler@mit.edu>
2017-06-07 08:39:23 +0200

    Last changes before JHEP resubmission

--- tex_jhep/arxivzip.sh
+++ tex_jhep/arxivzip.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-mkdir quarkgluon_v3
-mkdir quarkgluon_v3/figures/
+mkdir quarkgluon_v2p5
+mkdir quarkgluon_v2p5/figures/
 
-cp quarkgluon_jhep.tex quarkgluon_v3/
-cp quarkgluon_jhep.bbl quarkgluon_v3/
-cp jheppub.sty quarkgluon_v3/
+cp quarkgluon_jhep.tex quarkgluon_v2p5/
+cp quarkgluon_jhep.bbl quarkgluon_v2p5/
+cp jheppub.sty quarkgluon_v2p5/
 
 pushd figures
 for file in *.pdf
 do
    echo $file
-   cp $file ../quarkgluon_v3/figures/$file
+   cp $file ../quarkgluon_v2p5/figures/$file
 done
 popd
 
-pushd quarkgluon_v3/figures
+pushd quarkgluon_v2p5/figures
 for file in *.pdf
 do
    echo $file
@@ -25,10 +25,10 @@ do
 done
 popd
 
-tar cvzf quarkgluon_v3.tgz quarkgluon_v3/
+tar cvzf quarkgluon_v2p5.tgz quarkgluon_v2p5/
 
-pushd quarkgluon_v3
-pdflatex --file-line-error --synctex=1 quarkgluon_jhep.tex
-pdflatex --file-line-error --synctex=1 quarkgluon_jhep.tex
-pdflatex --file-line-error --synctex=1 quarkgluon_jhep.tex
-popd
+#pushd quarkgluon_v2p5
+#pdflatex --file-line-error --synctex=1 quarkgluon_jhep.tex
+#pdflatex --file-line-error --synctex=1 quarkgluon_jhep.tex
+#pdflatex --file-line-error --synctex=1 quarkgluon_jhep.tex
+#popd
